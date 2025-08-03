81b0cbd78ea9ed50246f0468c8f0d4b0ebcd710a
prakhar vishwakarma <prakharvision@gmail.com>
2025-08-03 19:27:06 +0530

    set up the redis database to perform the file compression and downloading the avatar image or other documents from the server or any other place where it could be located and then compress it and store it in the database

--- backend/src/workers/file_manager.ts
+++ backend/src/workers/file_manager.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv"
 import {db} from "../db/connection"
 import {user,type user as user_type} from "../db/schema"
-import client from "redis-cli"
+import client from "redis"
 dotenv.config
 ({
     path:"../.env"
+    
 })
 // we will run this program for infinite number of times
 while(true){
